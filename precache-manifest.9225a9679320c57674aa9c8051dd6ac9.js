3eefd62cf7a820dd813829cd7e2a39b44e2f14f1
MellYu <mel6273569@gmail.com>
2020-10-11 16:31:37 +0300

    Updates

---
+++ precache-manifest.9225a9679320c57674aa9c8051dd6ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefaf1790a5f56a9beff2e3ae804f887",
+    "url": "/react-homework-03-gallery/index.html"
+  },
+  {
+    "revision": "77290eb50038520ebdb4",
+    "url": "/react-homework-03-gallery/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "c227976c721aeaaf2220",
+    "url": "/react-homework-03-gallery/static/css/main.1fc346b8.chunk.css"
+  },
+  {
+    "revision": "77290eb50038520ebdb4",
+    "url": "/react-homework-03-gallery/static/js/2.ef33e2a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-homework-03-gallery/static/js/2.ef33e2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c227976c721aeaaf2220",
+    "url": "/react-homework-03-gallery/static/js/main.f42aedb0.chunk.js"
+  },
+  {
+    "revision": "9c5f6a735cc5c7fcd520",
+    "url": "/react-homework-03-gallery/static/js/runtime-main.5c847f08.js"
+  }
+]);
